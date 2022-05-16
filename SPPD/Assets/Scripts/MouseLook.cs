b63b579a31442ba17067ae3e89be7c30ee976f2a
KrisztianBajko <64569280+KrisztianBajko@users.noreply.github.com>
2022-05-16 15:22:07 +0100

    Simple FPS controller
    Mouse Look and player controller script

---
+++ SPPD/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    #region Public Fields
+
+
+    #endregion
+
+
+    #region Private Fields
+    [SerializeField] private float mouseSensitivity = 5f;
+    [SerializeField] private float xRotationClamp = 90f;
+    [SerializeField] Transform playerBody;
+
+
+    private float xRotation;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        LookAround();
+
+
+    }
+
+    #endregion
+
+
+
+    #region Public Methods
+
+
+    #endregion
+
+
+    #region Private Methods
+    private void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -xRotationClamp, xRotationClamp);
+
+        //rotate the camera on the x axes so we can look up and down
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+
+        // rotate the player body only
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    #endregion
+}
---
+++ SPPD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Public Fields
+
+
+    #endregion
+
+
+    #region Private Fields
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 15f;
+
+    [Header("Gravity / Jump")]
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float distanceToGround = .4f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheckGameObject;
+    private bool isGrounded;
+    private Vector3 velocity;
+
+
+    private CharacterController CC;
+    
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        CalculateGravity();
+        OnPlayerJump();
+        OnPlayerMove();
+        
+       
+    }
+
+    #endregion
+
+
+
+    #region Public Methods
+
+
+    #endregion
+
+
+    #region Private Methods
+    private void OnPlayerMove()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
+
+
+        CC.Move(move * moveSpeed * Time.deltaTime);
+    }
+    private void CalculateGravity()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheckGameObject.position, distanceToGround, groundLayer);
+
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        CC.Move(velocity * Time.deltaTime);
+    }
+    private void OnPlayerJump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+    #endregion
+}
